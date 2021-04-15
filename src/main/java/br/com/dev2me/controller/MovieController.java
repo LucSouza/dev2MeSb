ae2e795d622384be9215748855cbf328ceae4826
LucSouza <lsouza.prog@outlook.com>
2021-04-15 12:03:36 -0300

    add app.yaml for deploy on google cloud

--- src/main/java/br/com/dev2me/controller/MovieController.java
+++ src/main/java/br/com/dev2me/controller/MovieController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dev2me.entity.Movie;
+import br.com.dev2me.entity.MovieType;
 import br.com.dev2me.repository.MovieRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1")
 public class MovieController {
 	
@@ -25,14 +29,17 @@ public class MovieController {
 	private MovieRepository movieRepository;
 	
 	@GetMapping("/movies")
-	public List<Movie> getAll(){
+	public List<Movie> getAll(@RequestParam("movieType") MovieType movieType){
+		if (movieType== MovieType.ALL)
 		return movieRepository.findAll();
+		else
+		return movieRepository.findByMovieType(movieType);
 	}
 	
-	//ResponseEntity tive que usar Optional
 	@GetMapping("/movies/{id}")
-	public Optional<Movie> getById(@PathVariable(value = "id") Long movieId){
-		return movieRepository.findById(movieId);
+	public Movie getById(@PathVariable(value = "id") Long movieId){
+		Movie movie = movieRepository.findById(movieId).get();
+		return movie;
 	}
 	
 	@PostMapping("/movies")
@@ -42,8 +49,12 @@ public class MovieController {
 	//ResponseEntity tive que só Movie
 	@PutMapping("/movies/{id}")
 	public Movie update(@PathVariable(value = "id") Long movieId, @RequestBody Movie movieDetails){
-		return movieRepository.save(movieDetails);
+		Movie movie = movieRepository.findById(movieId).get();
+		movie = movieDetails;
+		return movieRepository.save(movie);
 	}
+	
+	
 	
 	@DeleteMapping("/movies/{id}")
 	public void deleteUser(@PathVariable(value = "id")Long MovieId) throws Exception{
